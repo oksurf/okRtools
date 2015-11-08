4e8cff3e55cda5b48ab872b5110d46ae24f1b64d
Oliver K <oksurf@yahoo.com>
2015-11-08 15:30:16 -0800

    Helloworld test

---
+++ R/helloworld.R
@@ -0,0 +1,12 @@
+#' A Hello World Function
+#'
+#' This is a test function
+#' @param none
+#' @keywords helloworld
+#' @export
+#' @examples
+#' helloworld()
+
+helloworld <- function() {
+  print("hello R world")
+}
